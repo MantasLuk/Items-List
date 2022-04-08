490fdcf35e641d0578bea8d9807d6ca450fd439f
Mantas Luk <mantasluk7@gmail.com>
2022-04-08 21:56:26 +0300

    Products list

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/ProductModel';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  public products:Product[]=[];
+
+  constructor() { 
+    let data = localStorage.getItem("products");
+    if(data != null){
+      this.products = JSON.parse(data);
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+  private save(){
+    localStorage.setItem("products", JSON.stringify(this.products));
+  }
+
+  public addNewItem(name:HTMLInputElement, count:HTMLInputElement){
+    if(name.value != ""){
+      this.products.push({
+        name: name.value,
+        count: count.valueAsNumber
+      });
+      name.value = '';
+      count.value = '';
+      this.save();
+    }
+  }
+
+  public deleteItem(i:number){
+    this.products.splice(i,1);
+    this.save();
+  }
+
+}
